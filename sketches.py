b152f44a0108ec9a3cc0d342fcac65ba5c855a99
adeldelvalle <adel.delvalle@upr.edu>
2024-04-08 22:28:19 -0400

    update

--- sketches.py
+++ sketches.py
@@ -1,4 +1,6 @@
 import mmh3
+import numpy as np
+import pandas as pd
 
 
 class Synopsis:
@@ -6,14 +8,14 @@ class Synopsis:
         self.table = table
         self.attributes = attributes
         self.key = key
-        attributes.append(key)
         self.attributes_values = table[attributes]
+        self.attributes_values[key] = table[key]
         self.sketch = self.preprocess()
 
     def preprocess(self):
         dic = {}
-        for row in self.attributes_values:
-            dic[self.f_hash(mmh3.hash128(row[self.key], 3))] = row[self.attributes]
+        for row in self.attributes_values.iterrows():
+            dic[self.f_hash(mmh3.hash128(row[self.key], 3))] = row
         return dic
 
     @staticmethod
@@ -25,3 +27,16 @@ class Synopsis:
         hash_value = (rescaled_key * golden_ratio_conjugate_frac) % 1
 
         return hash_value
+
+    def join_sketch(self, sketch_y, attr):
+        for key in self.sketch.keys():
+            if sketch_y.get(key) is not None:
+                self.sketch[key] = np.concatenate([self.sketch[key].values, sketch_y[key].values.flatten()])
+            else:
+                self.sketch[key] = np.concatenate([self.sketch[key].values, np.array([None] * attr)])
+
+
+
+
+
+
