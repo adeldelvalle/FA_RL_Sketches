1447fb281d45575e0f098fb9b013d181f09df8b0
adeldelvalle <adel.delvalle@upr.edu>
2024-04-18 20:53:11 -0400

    fixes and struct

--- data_prep.py
+++ data_prep.py
@@ -9,26 +9,30 @@ class Feature:
         self.corr_target_variable = None
         self.confidence_bound = None
 
+
 class Table:
     @classmethod
     def __init__(self, key, df):
-        #self.table = pd.read_csv("path")
+        # self.table = pd.read_csv("path")
         self.table = df
         self.rank = 0
         self.sketch = sketches.Synopsis(self.table, list(self.table.columns[1:]), key)
-        self.corr_matrix = sketches.Correlation(self.sketch)
         self.key = key
         self.feat_corr = {}
+        self.df_sketch = None
 
     def calc_corr_gain(self, y_synopsis):
+        y = y_synopsis.attributes[0]
         sketch_y = self.sketch.join_sketch(y_synopsis, 1)
+        self.df_sketch = pd.DataFrame(sketch_y.sketch.values(), columns=self.sketch.attributes)
+
         for feat in self.table.columns:
             if feat == self.key:
                 continue
             else:
                 feat_obj = Feature(feat)
                 feat_obj.corr_target_variable, feat_obj.confidence_bound = (
-                    sketches.Correlation(sketch_y).compute_parameters())
+                    sketches.Correlation(self.df_sketch[[feat, y]]).compute_parameters())
                 self.feat_corr[feat] = feat_obj
 
 
@@ -53,14 +57,15 @@ std2 = 10
 
 # Generate the first variable
 x = np.random.normal(mean1, std1, n)
+x1 = np.random.normal(mean1, std1, n)
 
 # Generate the second variable with some correlation to the first
 correlation = 0.30
 y = correlation * x + np.sqrt(1 - correlation ** 2) * np.random.normal(mean2, std2, n)
 
 # Create a DataFrame with these variables and the common key
-df = pd.DataFrame({'Key': keys, 'A': x, })
-df1 = pd.DataFrame({'Key': keys,  'B': y})
+df = pd.DataFrame({'Key': keys, 'A': x, 'C': x1})
+df1 = pd.DataFrame({'Key': keys, 'B': y})
 
 sketchy = Table('Key', df)
 sketchy_y = sketches.Synopsis(df1, ['B'], key='Key')
